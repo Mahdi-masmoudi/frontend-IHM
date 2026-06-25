10a5428684106fe62343d0ab68774dcf8a7c90e0
mahdi-masmoudi <mahdi.masmoudi92@gmail.com>
2026-06-26 00:27:24 +0100

    fix/api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiBaseUrl: 'http://localhost:4000'
+  apiBaseUrl: 'https://backend-ihm.vercel.app'
 };
